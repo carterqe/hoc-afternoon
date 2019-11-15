cfa09b8005a689e23880eab858d96727dde99cfb
Carter Endsley <carter.endsley@gmail.com>
2019-11-14 23:42:23 -0700

    i have finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import CurrencyHOC from './Components/CurrencyConverter'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <h2>Higher Order Component</h2>
+        <CurrencyHOC />
       </div>
     );
   }
